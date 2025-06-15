5c76116ec29a56d29e46ecb66dffd122571cb8ae
Ravinder <ravinder25886@gmail.com>
2025-06-15 12:17:10 +0100

    Pass Boolean, string,number and year in components

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import './App.css';
 import Header from './component/header/Header';
 import Footer from './component/footer/footer';
 
+// Define the data to pass
+const footerInfo = {
+  copyRight: "theravinder.com",
+  version: 1.2,
+  year:new Date().getFullYear()
+};
+
 function App() {
   return (
     <>
     <div className="App">
-     <Header title="I am from App!"/>
+     <Header isLoggedIn={true}/>
       <div>
         <h1>Home Page</h1>
       </div>
-    <Footer/>
+    <Footer footerData={footerInfo}/>
     </div>
     </>
   );
